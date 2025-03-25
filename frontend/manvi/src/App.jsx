acc3b42c83618797d863d768e5f5697e6ec53c51
Manvi027 <manvidadhwal@gmail.com>
2025-03-25 11:37:28 +0530

    react set-up

---
+++ frontend/manvi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div style ={{padding : '20px' , fontFamily: 'Arial,sans-serif'}}>
+        <h1>My name : Manvi Dadhwal</h1>
+        <p><strong>Email :</strong>manvidadhwal234@gmail.com</p>
+        <p><strong>Phone :</strong>1234567890</p>
+        </div>
+    );
+}
+
+export default App;
+
